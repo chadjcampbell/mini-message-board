4ce2e7caed73ac5f84658dd0a3b8b6cc820ff167
chadjcampbell <chadjcampbell@gmail.com>
2023-06-05 10:46:15 -0400

    add buttons and work on form data

--- routes/new.js
+++ routes/new.js
@@ -6,4 +6,13 @@ router.get("/", function (req, res, next) {
   res.render("form");
 });
 
+router.post("/", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  });
+  res.redirect("/");
+});
+
 module.exports = router;
